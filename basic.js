32dd5a62a5e292f95a47f3f5cd7805798b4c11ec
gall <mircofabio.gall@students.bfh.ch>
2024-02-18 19:59:41 +0100

    basic extraction from a webpage

--- basic.js
+++ basic.js
@@ -8,9 +8,21 @@ import puppeteer from 'puppeteer';
 // Imidiately invoked function
 //(()=>{})()
 // As soon as the page loads
-(async()=>{
-    const browser = await puppeteer.launch({headless:"new"});
+(async () => {
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
+    await page.setViewport({width: 1600, height: 1000, hasTouch: false, isLandscape: true,});
+
+    // navigating to page
+    await page.goto('https://www.digitec.ch/de/s1/product/xtorm-xb401-titan-24000-mah-60-w-8880-wh-powerbank-22612378');
+
+    // extracting content of the website
+    const url = await page.url();
+    const content = await page.content();
+    console.log(url);
+    console.log(content);
+    await page.screenshot({path: './screens/home-digitec.png', fullPage: true, type:'png'});
+
 
 
     await browser.close();
